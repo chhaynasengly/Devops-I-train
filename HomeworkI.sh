e02e0d20c37c02c1caa4839a40436a33e49cce5f
chhaynasengly <chhayna168@gmail.com>
2024-03-18 15:19:15 +0700

    HomeworkI.sh

---
+++ HomeworkI.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+echo "Please enter your folder name"
+read -p "Folder name: " fname
+
+Folder="$(echo $fname)"
+
+for ((a=2;a<=12;a++))
+
+do
+mkdir "LAB ${a}"
+
+done 
--- bash.sh
+++ bash.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!bin/bash
 echo "Please enter your name"
 read -p "First name: " fname
 read -p "Last name: " lname
 fullname="$(echo $fname $lname | tr 'a-z' 'A-Z')"
 echo "Hi $fullname"
-f=$(echo $fname | cut -c1 | tr 'A-Z' 'a-z')
+f=$(echo $fname | cut -c2 | tr 'A-Z' 'a-z')
 l=$(echo $lname | awk -F" " '{print $1}' | tr 'A-Z' 'a-z')
-domain="@dminc.com"
+domain="domain.com"
 echo "Email: $f$l@$domain"
