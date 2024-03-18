05c419d82bf82799a7d09a85ea43b082ffdbffee
chhaynasengly <chhayna168@gmail.com>
2024-03-18 18:27:52 +0700

    HomeworkII

--- HomeworkII.sh
+++ HomeworkII.sh
@@ -6,14 +6,14 @@ for ((i=2; i<=12; i++)); do
 
 
     # New folder name
-    new_folder="Folder$(printf "%01d" $i)"
+    new_folder="Folder$(printf "%02d" $i)"
 
 
     # Check if old folder exists
     if [ -d "$old_folder" ]; then
         # Rename folder
-        mv "$old_folder" "$new_folder"
-        echo "$old_folder renamed to $new_folder."
+        mv -- "$old_folder" "$new_folder"
+        echo "$old_folder renamed to $new_folder"
     else
         echo "$old_folder does not exist."
     fi
